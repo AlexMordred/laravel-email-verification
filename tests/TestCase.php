5337a8d0b04e8ae5393dbfa0bc9b2a3dcd9a60f6
Alexander Zavyalov <whmordred@gmail.com>
2018-01-29 17:15:05 +0800

    Removed the facade

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,7 +4,6 @@ namespace Voerro\Laravel\EmailVerification\Test;
 
 use Orchestra\Testbench\TestCase as OrchestraTestCase;
 use Voerro\Laravel\EmailVerification\EmailVerificationServiceProvider;
-use Voerro\Laravel\EmailVerification\EmailVerificationFacade;
 
 class TestCase extends OrchestraTestCase
 {
@@ -23,17 +22,5 @@ class TestCase extends OrchestraTestCase
     protected function getPackageProviders($app)
     {
         return [EmailVerificationServiceProvider::class];
-    }
-
-    /**
-     * Load package alias
-     * @param  \Illuminate\Foundation\Application $app
-     * @return array
-     */
-    protected function getPackageAliases($app)
-    {
-        return [
-            'EmailVerification' => EmailVerificationFacade::class,
-        ];
     }
 }
