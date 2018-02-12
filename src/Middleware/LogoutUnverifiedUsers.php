dd6335a8156cf8ce239b92a79ae1d4f6fccdc857
Alexander Zavyalov <whmordred@gmail.com>
2018-02-12 15:08:20 +0800

    Middleware adaptation to support API

--- src/Middleware/LogoutUnverifiedUsers.php
+++ src/Middleware/LogoutUnverifiedUsers.php
@@ -19,8 +19,15 @@ class LogoutUnverifiedUsers
         if (auth()->check() && !EmailVerification::userVerified(auth()->id())) {
             auth()->logout();
 
-            return redirect(config('email_verification.redirect_on_failure'))
-                ->with('status', __('email-verification::email_verification.middleware.redirect.message'));
+            if ($request->wantsJson()) {
+                return response()->json([
+                    'message' => __('email-verification::email_verification.middleware.redirect.message'),
+                    'status' => 409
+                ], 409);
+            } else {
+                return redirect(config('email_verification.redirect_on_failure'))
+                    ->with('status', __('email-verification::email_verification.middleware.redirect.message'));
+            }
         }
 
         return $next($request);
